e5b9e9d33f664b96e54a32c37359bd1a93022818
Tanvir <Tanvir>
2016-08-11 01:34:22 +0600

    modify venues code

--- 4sq/src/4sq_venues.py
+++ 4sq/src/4sq_venues.py
@@ -20,13 +20,13 @@ client = foursquare.Foursquare(client_id, client_secret)
 #Setting the Access Token to the Client
 #Access Token is for authentication
 client.set_access_token(access_token)
-
+venueID = input("Enter Venue ID: ")
 #venues ID is given as the parameter, here "4ca6e14d44a8224b17ef0740" is used as a demo
 #venue object is created here
-venues = client.venues('4ca6e14d44a8224b17ef0740')
+venues = client.venues(venueID)
 
 #for writing the Json data in the specified file
-with open('../data/Json/4sq_venues.json', 'a') as outfile:
+with open('../data/Json/4sq_%s_venues.json'%(venueID), 'a') as outfile:
     json.dump(venues, outfile)
 
 #for printing the Json data in the console 
